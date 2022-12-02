9bea3181e7aac9afc2182fd3454d042ffeed1aee
Dylan Hackworth <dylan.hackworth@homebase.ai>
2022-12-01 19:37:34 -0600

    added kill sync on error

--- src/api/sync.ts
+++ src/api/sync.ts
@@ -7,6 +7,7 @@ export type SyncOptions = {
     fullState?: boolean;
     setPresence?: MxApi.Set_presence;
     timeout?: number;
+    killOnError?: boolean;
 };
 
 export type SyncData = MxApi.Anonymous69;
@@ -30,23 +31,28 @@ export class SyncEntity extends Entity<SyncManager> {
 }
 
 export class SyncManager extends Manager<string, SyncEntity> {
+    private timers: number[] = [];
+
     private static get defaultPullTimeout() {
         return 10_000;
     }
 
-    private readonly options: SyncOptions & { timeout: number };
+    private readonly options: SyncOptions & { 
+        timeout: number,
+        killOnError: boolean,
+     };
 
     constructor(client: Client, holds: string, options: SyncOptions) {
         super(client, holds);
         this.options = {
             timeout: options.timeout ?? SyncManager.defaultPullTimeout,
+            killOnError: true,
             ...options,
         };
     }
 
     /** @internal */
     public sync(callback: (error?: Error, data?: SyncData) => void): void {
-        let timeoutId: NodeJS.Timer;
         const sync = async () => {
             // NOTE(dylhack): implicit object destruction to respect the inital
             //                next_batch passed to the constructor options.
@@ -62,17 +68,24 @@ export class SyncManager extends Manager<string, SyncEntity> {
                 this.options.since = resp.next_batch;
             } catch (error: unknown) {
                 if (error instanceof Error) callback(error);
-                clearTimeout(timeoutId);
             }
         };
 
         sync()
             .then(() => {
-                timeoutId = setInterval(sync, this.options.timeout);
+                const timeoutId = setInterval(sync, this.options.timeout);
+                if (typeof timeoutId === "number") this.timers.push(timeoutId);
+                else this.timers.push(timeoutId[Symbol.toPrimitive]());
             })
             .catch((error) => {
+                if (this.options.killOnError) this.killSync();
                 callback(error);
             });
+    }
+
+    public killSync() {
+        for (const t of this.timers) clearTimeout(t);
+        this.timers = [];
     }
 
     // eslint-disable-next-line @typescript-eslint/naming-convention
